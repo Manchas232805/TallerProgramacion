bed921648426778e03069d369694c834d2b217a0
Manchas232805 <manchas232805@gmail.com>
2024-09-22 08:45:43 -0500

    Update Ejercicio2.java

--- TareaS2/src/Ejercicio2.java
+++ TareaS2/src/Ejercicio2.java
@@ -1,4 +1,4 @@
-//Diferencia absoluta entre dos números donde el número 2 es el triple del primer número
+package Programas;
 import java.util.Scanner;
 import java.lang.Math;
 import java.text.DecimalFormat;
@@ -8,10 +8,13 @@ public class Ejercicio2 {
         //Declarar variables
         int num1, num2, numA;
         Scanner lectura = new Scanner(System.in);
+        //Entrada de datos
         System.out.print("Ingrese número 1: ");
         num1 = lectura.nextInt();
+        //Proceso de datos
         num2 = num1 * 3;
         numA = Math.abs(num1 - num2);
+        //Salida de datos
         System.out.println("El valor absoluto es: " + numA);
     }
 }
